7cd2c94f20ea30c2a8506597e279a1877130cf58
AxelSalim <adjakidjememiaghe@gmail.com>
2024-09-27 08:12:22 +0100

    mise en place du scrolle animation de la page

--- src/components/TextParallaxContent.jsx
+++ src/components/TextParallaxContent.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { motion, useScroll, useTransform } from "framer-motion";
 import { FiArrowUpRight } from 'react-icons/fi';
 
 export const TextParallaxContentExample = () => {
@@ -33,9 +34,15 @@ const IMG_PADDING = 12;
 
 const TextParallaxContent = ({ imgUrl, subheading, heading, children}) => {
   return(
-    <div>
+    <div
+      style={{
+        paddingLeft: IMG_PADDING,
+        paddingRight: IMG_PADDING,
+      }}
+    >
       <div className="relative h-[150vh]">
         <StickyImage imgUrl={imgUrl} />
+        <OverlayCopy heading={heading} subheading={subheading} />
       </div>
         {children}
     </div>
@@ -43,23 +50,60 @@ const TextParallaxContent = ({ imgUrl, subheading, heading, children}) => {
 }
 
 const StickyImage = ({ imgUrl }) => {
+
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["end end", "end start"],
+  });
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.85]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+
   return (
-    <div
+    <motion.div
       style={{
         backgroundImage: `url(${imgUrl})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         height: `calc(100vh - ${IMG_PADDING * 2}px)`,
         top: IMG_PADDING,
-        // scale,
+        scale,
       }}
-
+      ref={targetRef}
       className="sticky z-0 overflow-hidden rounded-3xl"
     >
-      <div />
-    </div>
+      <motion.div className="absolute inset-0 bg-neutral-950/70" />
+    </motion.div>
   );
 }
+
+
+const OverlayCopy = ({ subheading, heading }) => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start end", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [250, -250]);
+  const opacity = useTransform(scrollYProgress, [0.25, 0.5, 0.75], [0, 1, 0]);
+
+  return (
+    <motion.div
+      style={{
+        y,
+        opacity,
+      }}
+      ref={targetRef}
+      className="absolute left-0 top-0 flex h-screen w-full flex-col items-center justify-center text-white"
+    >
+      <p className="mb-2 text-center text-xl md:mb-4 md:text-3xl">
+        {subheading}
+      </p>
+      <p className="text-center text-4xl font-bold md:text-7xl">{heading}</p>
+    </motion.div>
+  );
+};
 
 
 const ExampleContent = () => (
